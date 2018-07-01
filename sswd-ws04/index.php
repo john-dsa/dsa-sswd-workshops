ea3e2a9864571ea161fd25bd3b0ea503d488eddb
John Botha <johannes.botha@digitalskillsacademy.me>
2018-07-01 17:14:22 +0200

    basic testing

--- sswd-ws04/index.php
+++ sswd-ws04/index.php
@@ -10,14 +10,14 @@ and open the template in the editor.
         <title>DSA-SSD Workshops</title>
     </head>
     <body>
-        <form action='index.php' method='GET'>
+        <form action='dice.class.php' method='POST'>
             <fieldset>
                 <legend>Dice Guess Input</legend>
                 <br>
                 <br>
                 <label>Guess 1: <input type='number' name='guess1' /></label>
                 <label>Guess 2: <input type='number' name='guess2' /></label>
-                <label>Guess 2: <input type='text' name='guess3' /></label>
+                <label>Guess 3: <input type='number' name='guess3' /></label>
                 <br>
                 <br>
                 <input type='submit' value='Submit Information' />
@@ -26,39 +26,42 @@ and open the template in the editor.
             </fieldset>
         </form>
         <?php
+
         //main code block
-        printf($guess1); echo "<br/>";
-        printf($guess2); echo "<br/>";
-        printf($guess3); echo "<br/>";
-        require_once 'Dice.class.php';
         require_once 'NumberGenerator.class.php';
+        require_once 'dice.class.php';
+        
         $dice = new Dice();
+        echo 'this is the dice: <br>';
+        echo '<pre>', print_r(($dice), 'true'), '</pre>'; //debug assist
         //load 3 guesses from user. at the moment pulling all 3 at the same tiem from the querystring..
-        $guess1 = $_GET['guess1'];
-        $guess2 = $_GET['guess2'];
-        $guess3 = $_GET['guess3'];
-        
-        print "Welcome to the Dice guessing program. Thanks for your guesses, lets see how you did .. <br/>";
-        
-        //object gues create
         $guess = new NumberGenerator();
-        echo '<pre>', print_r(($guess),'true'), '</pre>'; //debug assist
-        
-        $dice ->throwDice();
-        
-        switch ($dice->getFacevalue()){
-        case $guess1:
-            echo "Guess number one was correct!!";
-            break;
-        case $guess2:
-            echo "Guess number two was correct!!";
-            break;
-        case $guess3:
-            echo "Guess number three was correct!!";
-            break;
-        default :
-            echo "Sorry, you didn't make any correct guesses! The correct value was " . $dice -> getFacevalue();
+        echo 'this is the guess: <br>';
+        echo '<pre>', print_r(($guess), 'true'), '</pre>'; //debug assist
+        //throw the dice to get a dice value back
+        $dice->throwDice();
+        echo 'here is the post value';
+        echo '<pre>', print_r(($_POST), 'true'), '</pre>'; //debug assist
+        $guess1 = $_POST['guess1'];
+        $guess2 = $_POST['guess2'];
+        $guess3 = $_POST['guess3'];
+        print "Welcome to the Dice guessing program. Thanks for your guesses, lets see how you did .. <br/>";
+       //check dice against the guess
+        switch ($dice->getFacevalue()) {
+            case $guess1:
+                echo "Guess number one was correct!!";
+                break;
+            case $guess2:
+                echo "Guess number two was correct!!";
+                break;
+            case $guess3:
+                echo "Guess number three was correct!!";
+                break;
+            default :
+                echo "Sorry, you didn't make any correct guesses! The correct value was " . $dice->getFacevalue();
         }
+ 
         ?>
     </body>
 </html>
+
