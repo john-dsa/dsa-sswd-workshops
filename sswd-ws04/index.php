4439d50492b4204c98300802229096fc7075dcb7
john-dsa <johhannes.botha@digitalskillsacademy.me>
2018-07-14 11:00:18 +0200

    ver11

--- sswd-ws04/index.php
+++ sswd-ws04/index.php
@@ -1,67 +1,81 @@
+<?php 
+	/*
+		index
+		SSWD Worskshops: mySQL Databases III | the Dice Game
+		
+		Rev.1
+
+		Date 13.05.2015
+		@author John Botha
+	*/
+?>
+
+
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>DSA-SSD Workshops</title>
-    </head>
-    <body>
-        <form action='dice.class.php' method='POST'>
-            <fieldset>
-                <legend>Dice Guess Input</legend>
-                <br>
-                <br>
-                <label>Guess 1: <input type='number' name='guess1' /></label>
-                <label>Guess 2: <input type='number' name='guess2' /></label>
-                <label>Guess 3: <input type='number' name='guess3' /></label>
-                <br>
-                <br>
-                <input type='submit' value='Submit Information' />
-                <br>
-                <br>
-            </fieldset>
-        </form>
-        <?php
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>SSWD Worskshops - mySQL Databases III | the Dice Game </title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
 
-        //main code block
-        require_once 'NumberGenerator.class.php';
-        require_once 'dice.class.php';
-        
-        $dice = new Dice();
-        echo 'this is the dice: <br>';
-        echo '<pre>', print_r(($dice), 'true'), '</pre>'; //debug assist
-        //load 3 guesses from user. at the moment pulling all 3 at the same tiem from the querystring..
-        $guess = new NumberGenerator();
-        echo 'this is the guess: <br>';
-        echo '<pre>', print_r(($guess), 'true'), '</pre>'; //debug assist
-        //throw the dice to get a dice value back
-        $dice->throwDice();
-        echo 'here is the post value';
-        echo '<pre>', print_r(($_POST), 'true'), '</pre>'; //debug assist
-        $guess1 = $_POST['guess1'];
-        $guess2 = $_POST['guess2'];
-        $guess3 = $_POST['guess3'];
-        print "Welcome to the Dice guessing program. Thanks for your guesses, lets see how you did .. <br/>";
-       //check dice against the guess
-        switch ($dice->getFacevalue()) {
-            case $guess1:
-                echo "Guess number one was correct!!";
-                break;
-            case $guess2:
-                echo "Guess number two was correct!!";
-                break;
-            case $guess3:
-                echo "Guess number three was correct!!";
-                break;
-            default :
-                echo "Sorry, you didn't make any correct guesses! The correct value was " . $dice->getFacevalue();
-        }
- 
-        ?>
-    </body>
+	<body>
+		<div id="container">	
+			<header>
+				<?php
+					// add random images to 'welcome' heading
+					$logoSrcArray = array("images/roll1.jpg", "images/roll2.jpg", "images/roll3.jpg");
+
+					// cast images from array
+					$castImgSrc1 = $logoSrcArray[rand(0, sizeOf($logoSrcArray)-1)];		// cast source of the left image
+					$castImgSrc2 = $logoSrcArray[rand(0, sizeOf($logoSrcArray)-1)];		// cast source of the right image
+
+					// add the style to casted images
+					$img1 = "<img class='headerImg' src=".$castImgSrc1." alt='logo1' >";
+					$img2 = "<img class='headerImg' src=".$castImgSrc2." alt='logo2' >";
+
+					// add images into heading
+					echo "<h1>" . $img1 . " Welcome to the Dice guessing program " . $img2 . "</h1>";
+				
+				?>
+			</header>
+
+			<form action="index.php" method="POST">
+				<input type="submit" name="guess" id="guessBtn" value="Make your guess">
+			</form>
+
+
+			<?php 
+				include ('NumberGenerator.Class.php');
+				include ('Dice.Class.php');
+				include ('Game.Class.php');
+				include ('MyDB.Class.php');
+
+				/*
+				// OR instead of lines above:
+				function __autoload($class_name) {	// it's a good practice for complex apps
+					require "./".$class_name.".Class.php";
+				}
+				*/
+
+				// start the game
+				if(isset($_POST['guess'])) {
+					echo "<h3>Thanks for the guesses, lets see the result...<br/></h3>";
+					$myGame = new Game();
+					$myGame->play();
+				}
+
+				// reset the score table (delete all records in 'correct_guesses' table)
+				echo "<br><form action='index.php' method='POST'>
+					<input type='submit' name='resetTable' id='resetBtn' value='Reset All Scores & Start New Game'>
+				</form>";
+
+				if(isset($_POST['resetTable'])) {
+					$db = new MyDB("localhost", "root", "", "my_db");
+					$db->resetScores();
+				}
+			?>
+		</div>
+		<footer >SSWD Worskshops | @ John Botha, 2018</footer>
+	</body>
 </html>
-
