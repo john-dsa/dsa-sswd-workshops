f7c92d2b4d3d4494bef06be4aa480b408fcd6c7e
john-dsa <johhannes.botha@digitalskillsacademy.me>
2018-07-14 14:35:37 +0200

    ver17

--- sswd-ws04/MyDB.Class.php
+++ sswd-ws04/MyDB.Class.php
@@ -1,101 +1,93 @@
-<?php 
-	/*
-		MyDB.Class
-		TE20_C_FSD - Workshop 12: mySQL Databases III | the Dice Game
+<?php
 
-		- description:
-			the program cooperates with the 'my_db' database:
-				table: correct_guesses,
-				fields: 
-					- user_id
-					- answer
-					- correct_guesses_count
-					- guessed_on
+/*
+  MyDB.Class
+   SSWD Worskshops the Dice Game
 
-		- class data:
-			$dbConn;			// database connection
-			$dbQuery;			// database query
-			$conn_error;		// connection error
+  - description:
+  the program cooperates with the 'my_db' database:
+  table: correct_guesses,
+  fields:
+  - user_id
+  - answer
+  - correct_guesses_count
+  - guessed_on
 
-		- class methods:
-			__construct()		// a constructor, sets variables & connection to database
-			getConnError()		// returns connection error
-			getTable()			// displays database's output as a table
-			saveToDb()			// saves current guesses in database
-			resetScores()		// delete all records from correct_guesses table
+  - class data:
+  $dbConn;			// database connection
+  $dbQuery;			// database query
+  $conn_error;		// connection error
 
-		Rev.1
+  - class methods:
+  __construct()		// a constructor, sets variables & connection to database
+  getConnError()		// returns connection error
+  getTable()			// displays database's output as a table
+  saveToDb()		// saves current guesses in database
+  resetScores()		// delete all records from correct_guesses table
 
-		Date 17.05.2015
-		@author Agnieszka Pas
-	*/
+   Rev.1
+  Date 14.07.2018 Author John Botha
+  Adapted by original work from Agnieszka Pas https://github.com/agapas/mini-dice-game.git
+ */
 
+error_reporting(0);  // turn off reporting of mySQL errors
 
-	error_reporting(0);		// turn off reporting of mySQL errors
+class MyDB {
+    private $dbConn;
+    private $dbQuery; //container for queries
+    private $conn_error;
 
+    public function __construct($host, $userName, $password, $dbName) {
+        //below dbconn gets re-used with ell methods whenever the db need to be touched
+        $this->dbConn = mysqli_connect($host, $userName, $password, $dbName) or die("Error..." . mysqli_connect_error($this->dbConn));
+    }
 
-	class MyDB {
+    // get connection error for db connection
+    public function getConnError() {
+        return $this->conn_error = "Error..." . mysqli_error($this->dbConn);
+    }
 
-		private $dbConn;
-		private $dbQuery;
-		private $conn_error;
+    // save scores to database
+    public function saveToDb($guess, $guessesCount) {
+        $guessed_on = date("Y-m-d");
+        //set the content of the dbquery
+        $this->dbQuery = "INSERT INTO correct_guesses(answer, correct_guesses_count, guessed_on) VALUES ('" . $guess . "'," . "'" . $guessesCount . "'," . "'" . $guessed_on . "')";
+        $result = $this->dbConn->query($this->dbQuery) or die($this->getConnError());
+    }
 
-		public function __construct($host, $userName, $password, $dbName) {
-			$this->dbConn = mysqli_connect($host, $userName, $password, $dbName) or die("Error...".mysqli_connect_error($this->dbConn));
-		}
+    // delete all records from correct_guesses table - called when the users posts form button "reset game"
+    public function resetScores() {
+        $this->dbQuery = "TRUNCATE TABLE correct_guesses"; //set the content of the dbquery
+        $result = $this->dbConn->query($this->dbQuery) or die($this->getConnError()); //note the method chaining
+    }
 
-		// get connection error
-		public function getConnError() {
-			return $this->conn_error = "Error...".mysqli_error($this->dbConn);
-		}
+    // display scores as a table
+    public function getTable() {
+        // create the query to retrieve all data
+        $this->dbQuery = "SELECT * FROM correct_guesses";
+        // execute the query
+        $result = $this->dbConn->query($this->dbQuery) or die($this->getConnError());
+        echo "----------------------------------------------------------";
+        echo "<h3>Your Score:</h3>";
+        echo "<table cellpadding='4'>";
+        echo "<tr id='tableTitle'>";
+        echo "<th>round<br>number</th>";
+        echo "<th>value<br>of the dice</th>";
+        echo "<th>correct guesses count<br>per round</th>";
+        echo "<th>guessed<br>on</th>";
+        echo "</tr>";
 
-		// save scores to database
-		public function saveToDb($guess, $guessesCount) {
-			$guessed_on = date("Y-m-d");
-
-			$this->dbQuery = "INSERT INTO correct_guesses(answer, correct_guesses_count, guessed_on) VALUES ('".$guess."',"."'".$guessesCount."',"."'".$guessed_on."')";
-
-			$result = $this->dbConn->query($this->dbQuery) or die($this->getConnError());
-		}
-
-		// delete all records from correct_guesses table
-		public function resetScores() {
-			$this->dbQuery = "TRUNCATE TABLE correct_guesses";
-			$result = $this->dbConn->query($this->dbQuery) or die($this->getConnError());
-		}
-
-		// display scores as a table
-		public function getTable() {
-			// create the query to retrieve all data
-			$this->dbQuery = "SELECT * FROM correct_guesses";
-
-			// execute the query
-			$result = $this->dbConn->query($this->dbQuery) or die($this->getConnError());
-
-			echo "----------------------------------------------------------";
-			echo "<h3>Your Score:</h3>";
-
-			echo "<table cellpadding='4'>";
-			
-			echo "<tr id='tableTitle'>";
-			echo "<th>round<br>number</th>";
-			echo "<th>value<br>of the dice</th>";
-			echo "<th>correct guesses count<br>per round</th>";
-			echo "<th>guessed<br>on</th>";
-			echo "</tr>";
-
-			while($row = $result->fetch_assoc()) {
-				echo "<tr>";
-				echo "<td>".$row["user_id"]."</td>";
-				echo "<td>".$row["answer"]."</td>";
-				echo "<td>".$row["correct_guesses_count"]."</td>";
-				echo "<td>".$row["guessed_on"]."</td>";
-				echo "</tr>";
-			}
-			echo "</table>";
-
-			// free memory associated with result
-			mysqli_free_result($result);
-		}
-	}
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $row["user_id"] . "</td>";
+            echo "<td>" . $row["answer"] . "</td>";
+            echo "<td>" . $row["correct_guesses_count"] . "</td>";
+            echo "<td>" . $row["guessed_on"] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        // free memory associated with result
+        mysqli_free_result($result);
+    } //end table
+} //end class
 ?>
--- sswd-ws04/index.php
+++ sswd-ws04/index.php
@@ -13,7 +13,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>TE20_C_FSD Workshop 12 - mySQL Databases III | the Dice Game </title>
+        <title>DSA SSWD - mySQL Databases - the Dice Game </title>
         <link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
 
@@ -40,7 +40,6 @@
                 <input type="submit" name="guess" id="guessBtn" value="Make a guess">
             </form>
 
-
 <?php
 //start running the php, call the classes
 include ('NumberGenerator.Class.php'); //to gues a random number 1 to 6
@@ -49,10 +48,8 @@ include ('Game.Class.php');      // bit of a god class. will play the game.
                        //instantiate a dice object,
                             //action the dice object throwDice method   
                             //assign a diceValue from the dice object getFaceValue method, after the throwDice
-                            //
     
 include ('MyDB.Class.php');
-
 /*
   // OR instead of lines above:
   function __autoload($class_name) {	// it's a good practice for complex apps
@@ -67,17 +64,17 @@ if (isset($_POST['guess'])) {
     $myGame->play();        //object play method is executed. it will also 
 }
 
-// reset the score table (delete all records in 'correct_guesses' table)
-echo "<br><form action='index.php' method='POST'>
-					<input type='submit' name='resetTable' id='resetBtn' value='Reset All Scores & Start New Game'>
-				</form>";
+// reset the score table (delete all records in 'correct_guesses' table) when the user clicks(post) the reset button
+echo    "<br><form action='index.php' method='POST'>
+       <input type='submit' name='resetTable' id='resetBtn' value='Reset All Scores & Start New Game'>
+       </form>";
 
 if (isset($_POST['resetTable'])) {
     $db = new MyDB("localhost", "root", "", "my_db");
     $db->resetScores();
 }
 ?>
-        </div>
-        <footer >TE20_C_FSD, Workshop 12 | Made by @ Agnieszka Pas, 2015</footer>
+    </div>
+    <footer>DSA WSSD @ John Botha, 2018</footer>
     </body>
 </html>
